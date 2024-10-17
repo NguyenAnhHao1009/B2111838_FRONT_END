702073402d8ec9c7b96cb4ffb4b6adafd737d77e
NguyenAnhHao1009 <anhhaoaoao@gmail.com>
2024-10-17 20:10:58 +0700

    Dinh nghia route va trang not found

--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,11 @@ const routes = [
     name: "contactbook",
     component: ContactBook,
   },
-    {
-      path: "/add",
-      name: "contact.add",
-      component: ContactBook,
-    },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="text-center">
+    <p>
+      Oops, không thể tìm thấy trang. Trở về
+      <router-link to="/">trang chủ.</router-link>
+    </p>
+  </div>
+</template>
